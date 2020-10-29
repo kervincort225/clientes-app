2ff0322fe6e87f23c91b18bc74f4aa7111bd33e8
kervin cortez <kervin.cortez@pad.cl>
2020-10-29 12:04:22 -0300

    creando archivo clientes.json.ts con la lista de objetos

--- src/app/clientes/clientes.component.ts
+++ src/app/clientes/clientes.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { Cliente } from './cliente';
+import { CLIENTES } from './clientes.json';
 
 @Component({
   selector: 'app-clientes',
@@ -7,21 +8,12 @@ import { Cliente } from './cliente';
 })
 export class ClientesComponent implements OnInit {
 
-  clientes: Cliente[] = [
-    {id: 1 , nombre:'kervin', apellido:'cortez', email:'kervincort225@gmail.com', createAt:'2020-10-29'},
-    {id: 2 , nombre:'jober', apellido:'cortez', email:'jobercort225@gmail.com', createAt:'2020-10-29'},
-    {id: 3 , nombre:'xiomar', apellido:'tovar', email:'xiomarcort225@gmail.com', createAt:'2020-10-29'},
-    {id: 4 , nombre:'eris', apellido:'cortez', email:'eriscort225@gmail.com', createAt:'2020-10-29'},
-    {id: 5 , nombre:'jobeyker', apellido:'cortez', email:'jobeykercort225@gmail.com', createAt:'2020-10-29'},
-    {id: 6 , nombre:'vanessa', apellido:'cortez', email:'vanessacort225@gmail.com', createAt:'2020-10-29'},
-    {id: 7 , nombre:'marielisa', apellido:'chamorro', email:'marielisacort225@gmail.com', createAt:'2020-10-29'},
-    {id: 8 , nombre:'yeanellis', apellido:'ulacio', email:'yeanelliscort225@gmail.com', createAt:'2020-10-29'},
-    {id: 9 , nombre:'genesis', apellido:'celaya', email:'genesiscort225@gmail.com', createAt:'2020-10-29'}
-  ];
+  clientes: Cliente[];
 
   constructor() { }
 
   ngOnInit(): void {
+    this.clientes = CLIENTES;
   }
 
 }
